e340acd4fbd327bde7cc5e5c9df6a765c95b8cec
violet <vdelarosa210@gmail.com>
2017-04-12 13:59:16 -0500

    Completed iterating array exercise with forEach function

--- js/iterating.js
+++ js/iterating.js
@@ -8,17 +8,30 @@
 
 
 
-    console.log('There are ' + names.length + ' names in the array');
+    // console.log('There are ' + names.length + ' names in the array');
 
-    console.log('The first name is: ' + names [0]);
+    // console.log('The first name is: ' + names [0]);
+    //
+    // console.log('The second name is: ' + names [1]);
+    //
+    // console.log('The third name is: ' + names [2]);
+    //
+    // console.log('The fourth name is: ' + names [3]);
+    //
+    // console.log('');
+    //
+    // console.log('For Loop:');
 
-    console.log('The second name is: ' + names [1]);
+    // for (var i = 0; i < names.length; ++i) {
+    //     console.log(names[i]);
+    // }
 
-    console.log('The third name is: ' + names [2]);
-
-    console.log('The fourth name is: ' + names [3]);
-
+    names.forEach(function(element, index) {
+        console.log('The name at index ' + index + ' is: ' + element);
+    })
 })();
+
+
 
 // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
 
