7fcd6b3b6e3d87957aaf00f2125b8daf7437936d
violet <vdelarosa210@gmail.com>
2017-04-12 12:12:00 -0500

    Completed iterating array exercise with repetition console.logs

---
+++ js/iterating.js
@@ -0,0 +1,27 @@
+/**
+ * Created by violet on 4/12/17.
+ */
+(function(){
+    "use strict";
+
+    var names = ['Tom', 'Jerry', 'Bugs', 'Bunny'];
+
+
+
+    console.log('There are ' + names.length + ' names in the array');
+
+    console.log('The first name is: ' + names [0]);
+
+    console.log('The second name is: ' + names [1]);
+
+    console.log('The third name is: ' + names [2]);
+
+    console.log('The fourth name is: ' + names [3]);
+
+})();
+
+// TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
+
+// TODO: Create a log statement that will log the number of elements in the names array.
+
+// TODO: Create log statements that will print each of the names array elements individually.
