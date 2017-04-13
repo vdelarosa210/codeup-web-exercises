5ef9dd371b462b7d7b6fc4a4a106d1c1c93cdd2b
violet <vdelarosa210@gmail.com>
2017-04-13 10:36:09 -0500

    Added break to array

---
+++ js/planet-strings.js
@@ -0,0 +1,28 @@
+/**
+ * Created by violet on 4/13/17.
+ */
+(function(){
+    "use strict";
+
+    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+    console.log(planetsString);
+
+    var planetsArray = planetsString.split('|');
+    console.log(planetsArray);
+
+    for (var i = 0; i < planetsArray.length; i++){
+        document.write(planetsArray[i] + '<br />')
+    }
+
+    // TODO: Convert planetsString to an array, save it to planetsArray.
+
+
+    // TODO: Create a string with <br> tags between each planet. console.log() your results.
+    //       Why might this be useful?
+
+
+
+    // Bonus: Create a second string that would display your planets in an undordered list.
+    //        You will need an opening AND closing <ul> tags around the entire string, and <li> tags around each planet.
+    //        console.log() your results.
+})();
