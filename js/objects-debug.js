f79bc09de73e8d6428026bdafd6b754678611ce8
violet <vdelarosa210@gmail.com>
2017-04-14 09:54:12 -0500

    Completed final section of debugging by correcting console.log requests

--- js/objects-debug.js
+++ js/objects-debug.js
@@ -30,73 +30,76 @@ console.log('The type of crust for the first pizza: ' + pizza.crust);
 
 console.log(pizza.crust);
 
-})();
+
 
 //////////////////////////////////////////////////////////
 // When you are finished with the above part, uncomment //
 // everything until the next comment block              //
 //////////////////////////////////////////////////////////
 
-// orders = [
-//     {
-//         toppings: ['pepperoni', 'green peppers', 'onions'],
-//         crust: 'stuffed',
-//         delivery: true,
-//         price: 1295,
-//     },
-//     {
-//         toppings: ['onion', 'salami'],
-//         crust: 'pretzel',
-//         delivery: false,
-//         price: 1195,
-//     },
-//     {
-//         toppings: ['pineapple', 'ham'],
-//         crust: 'regular',
-//         delivery: true
-//         specialInstructions: 'extra crispy crust',
-//         price: 995,
-//     }
-// ];
-//
-// console.log('Here are the different crust types for each order: ');
-// orders.forEach(function(order) {
-//     console.log(orders.crust)
-// });
+var orders = [
+    {
+        toppings: ['pepperoni', 'green peppers', 'onions'],
+        crust: 'stuffed',
+        delivery: true,
+        price: 12.95
+    },
+    {
+        toppings: ['onion', 'salami'],
+        crust: 'pretzel',
+        delivery: false,
+        price: 11.95
+    },
+    {
+        toppings: ['pineapple', 'ham'],
+        crust: 'regular',
+        delivery: true,
+        specialInstructions: 'extra crispy crust',
+        price: 9.95
+    }
+];
+    console.log('Here are the different crust types for each order: ' );
+orders.forEach(function(order){
+    console.log(order.crust);
+    })
+
+
 
 //////////////////////////////////////////////////////////////////////
 // When you are finished with the section above, uncomment the rest //
 //////////////////////////////////////////////////////////////////////
 
-// console.log('--------------------------------------------------');
-// console.log(' > Orders For Delivery');
-// console.log('--------------------------------------------------');
-//
-// // output information about all the orders for delivery
-// for (var index = 0; index <= orders.length; index += 1) {
-//     var order = orders[index];
-//
-//     // if an order is not for delivery, skip it
-//     if (! order.delivery) {
-//         break;
-//     }
-//
-//     var orderNumber = index + 1;
-//     var formattedPrice = '$' + (order.price / 100).toFixed(2);
-//
-//     // output the order number
-//     console.log('--- Order # ' + orderNumber + ' ---');
-//     console.log('Toppings');
-//     // output all the toppings for the order
-//     order.toppings.forEach(function (topping) {
-//         console.log('- ' + order.topping);
-//     });
-//
-//     console.log('Crust: ' + order.crust);
-//     // output the special instructions, if there are any
-//     if (order.specialInstructions) {
-//         console.log('Special Prep: ' + order.specialInstructions);
-//     }
-//
-//     console.log('Total: ' + formattedPrice);
-// }
+console.log('--------------------------------------------------');
+console.log(' > Orders For Delivery');
+console.log('--------------------------------------------------');
+
+// output information about all the orders for delivery
+for (var index = 0; index <= orders.length; index += 1) {
+    var order = orders[index];
+
+    // if an order is not for delivery, skip it
+    if (! order.delivery) {
+        break;
+    }
+
+    var orderNumber = index + 1;
+    var formattedPrice = '$' + (order.price / 100).toFixed(2);
+
+    // output the order number
+    console.log('--- Order # ' + orderNumber + ' ---');
+    console.log('Toppings' + order.toppings.length);
+    // output all the toppings for the order
+    order.toppings.forEach(function(topping) {
+        console.log(' - ' + topping);
+    });
+
+    console.log('Crust: ' + order.crust);
+    // output the special instructions, if there are any
+    if (order.specialInstructions) {
+        console.log('Special Prep: ' + order.specialInstructions);
+    }
+
+    console.log('Total: ' + formattedPrice);
+}
+
+})();
