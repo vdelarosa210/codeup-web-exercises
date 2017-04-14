bc6a5796b81ed2f59b28d2212e8f0dbdf7491bfe
violet <vdelarosa210@gmail.com>
2017-04-14 09:14:24 -0500

    Debugged first section by adding IIFE, removed unneeded comma after true and corrected console.log statement by adding + pizza.crust

--- js/if-else.js
+++ js/if-else.js
@@ -3,83 +3,17 @@
  */
 'use strict';
 
-console.log("Student is successful")
-
-
-
-if (success) {
-    message = "You're awesome.";
-} else if {
-    message = "You need to practice more.";
-} else if () {
-
-} else {
-
-} else {
-
+var one = 70, two = 80, three = 95;
+var average = (one + two + three) / 3;
+console.log(average);
+if (average > 80){
+    console.log("You're Awesome");
+}
+else {
+    console.log("You need to practice more");
 }
 
-
-
-var x = 1
-
-if (x=1) {
-    console.log("Condition 1");
-} else if (x = 2) {
-    console.log("Condition 2");
-} else {
-    console.log("None of the other conditions were true")
-
-    var message = (condition) 7 executed when true : the else part;
-
-    //PROBLEM 2 ==========
-
-    // define a variable for the name of the customer:
-
-    var customer = "";
-
-    // define a variable for the total amount charged
-
-    var costDollars = null;
-
-    // define a variable for the discount rate
-    var discountRate =.35;
-
-    // define a variable for the total amount purchased
-    var totalSpentDollars = null;
-
-    //define a variable for the discount breakpoint (the minimum amount spent to apply)
-    var discountBreakpointDollars = 200;
-
-    //define a variable for each customer's purchase
-    var cameronSpentDollars = 180;
-    var ryanSpentDollars = 250;
-    var georgeSpentDollars = 320;
-
-    var cameron = "Cameron";
-    var ryan = "Ryan";
-    var george = "George";
-
-    // the following logic will be repeated for each customer (3 times total)
-    // asign the total spent to the amount the fist customer spent
-
-    customer = cameron;
-    totalSpentDollars = cameronSpentDollars;
-
-    if (totalSpentDollars > discountBreakpointDollars) {
-        costDollars = totalSpentDollars - (totalSpentDollars * discountRate);
-    } else {
-        costDollars = totalSpentDollars;
-    }
-
-    // if the total amount spent is greater than the break point, apply the discount and assign value to amount charged
-
-// else assign the value spent to the amount charged
-
-// output to the console the name of the customer, how much they spent, and what they were charged.
-
-console.log(customer + "bought" + "$" + totalSpentDollars + "worth of products." +  "Final payment: " + "$" + costDollars
-
-
-
-
+var amount = [180, 250, 320];
+var clients = ["Cameron", "Ryan", "George"] ;
+amount.forEach(function (element)
+if (amount > 200)
